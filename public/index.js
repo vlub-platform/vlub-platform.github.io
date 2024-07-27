6c3be937358e0df7a88c213be9571ed3d0dc5a2f
game-platform <vlublyash@gmail.com>
2024-07-28 01:56:07 +0300

    upload v0.2

---
+++ public/index.js
@@ -0,0 +1,24 @@
+let processplant = 1;
+let imgplant = "claim0"+processplant+".png";
+
+function entplant() {
+	if(processplant < 5) {
+		processplant += 1;
+	}
+	if(processplant > 4) {
+		processplant = 1;
+	}
+	imgplant = "claim0"+processplant+".png"
+	document.getElementById("plant").src = "./public/"+imgplant;
+}
+
+
+
+function claimplant() {
+	if(processplant < 4) {
+		console.log(`error`);
+	}
+	if(processplant == 4) {
+		console.log(`claim`);
+	}
+}
